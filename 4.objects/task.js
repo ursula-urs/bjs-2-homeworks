4ad338639ab8ce830c9d63b7aa5a6b3f1332b5c5
Ursula <5lyusita@gmail,com>
2026-04-21 21:52:52 +0300

    homework 4

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,35 @@
 function Student(name, gender, age) {
-  
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+  this.subject = subjectName; 
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+    if (!this.marks){ 
+        return;
+    }
+    this.marks.push(...marks);
 }
 
 Student.prototype.getAverage = function () {
-  
+    if(!this.marks.length){
+        return 0;
+    }else{
+        let sum = 0;
+        for( let i=0; i < this.marks.length; i++){
+            sum += this.marks[i];
+        }
+        return sum / this.marks.length;
+    }
 }
 
 Student.prototype.exclude = function (reason) {
-  
+    delete this.subject;
+    delete this.marks;
+    this.excluded = reason;
 }
