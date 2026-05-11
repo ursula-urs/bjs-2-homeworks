fa502b39c37b7ed0a87a3fe6954035caf67d27a6
Ursula <5lyusita@gmail,com>
2026-05-11 19:56:31 +0300

    homework

--- 7.async/task.js
+++ 7.async/task.js
@@ -8,5 +8,66 @@ class AlarmClock{
     if(!time || !callback){
         throw new Error('Отсутствуют обязательные аргументы');
     }
+    
+    const exists = this.alarmCollection.some(
+        alarm => alarm.time === time);
+
+        if(exists){
+        console.warn('Уже присутствует звонок на это же время');
+    }
+
+
+    this.alarmCollection.push({
+        time, callback, canCall: true
+    });
 }
+
+ removeClock(time) {
+    this.alarmCollection = this.alarmCollection.filter(
+      alarm => alarm.time !== time);
+}
+
+getCurrentFormattedTime(){
+    const date = new Date();
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    return '${hours}:${minutes}';
+}
+
+start(){
+    if(this.intervalId){
+        return;
+    }
+
+      this.intervalId = setInterval(() => {
+            const currentTime = this.getCurrentFormattedTime();
+            this.alarmCollection.forEach(alarm => {
+                if (
+                    alarm.time === currentTime &&
+                    alarm.canCall === true
+                ) {        
+                    alarm.canCall = false;
+
+                    alarm.callback();
+                }
+            });
+
+        }, 1000);
+    }
+
+    stop() {
+        clearInterval(this.intervalId);
+        this.intervalId = null;
+    }
+
+    resetAllCalls() {
+        this.alarmCollection.forEach(alarm => {
+            alarm.canCall = true;
+        });
+    }
+
+    clearAlarms() {
+        this.stop();
+        this.alarmCollection = [];
+    }
 }
